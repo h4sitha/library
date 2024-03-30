7271df36f24b80aea68547d4538a8cefd8e77ced
Hasitha Ekanayake <150508298+h4sitha@users.noreply.github.com>
2024-03-30 14:38:40 +0530

    Add function to add books to the library array

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+const libraryBooks = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+}
+
+function addBookToLibrary() {
+    const newBook = new Book();
+    libraryBooks.push(newBook);
 }
